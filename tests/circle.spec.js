a8e0eea830e447e47e9a3ae1bcf25ecd01104538
claytongom <claytoncnx@gmail.com>
2022-10-20 19:05:13 -0300

    resolução requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -19,18 +19,27 @@ const circle = require('../src/circle');
         Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
         Outra dica: que tal pesquisar se existe um matcher que compara valores próximos?
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
-*/
 
+        */
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle(!Number)).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(typeof circle(3)).toBe('object');
     // Teste se o objeto retornado possui 3 propriedades.
+    expect(Object.keys(circle(3)).length).toEqual(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle('')).toBe(undefined);
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
+    expect(Object.values(circle(2))[2]).toBe(12.56);
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
+    expect(circle(3).area).toBeCloseTo(28.26, 2);
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+    expect(Object.values(circle(3))[0]).toBeCloseTo(3)
+    expect(Object.values(circle(3))[1]).toBeCloseTo(28.26)
+    expect(Object.values(circle(3))[2]).toBeCloseTo(18.84)
+
   });
 });
